2dc559dcc0c1a35eca6d5548337d39cc08ab9fd5
eliza124 <49456953+eliza124@users.noreply.github.com>
2020-12-25 14:42:48 +0800

    Add files via upload

---
+++ 01_Solution.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 15:24:00 2019
+
+@author: eliza124
+"""
+
+## Exercise INT1A
+print("Eliza Wan")
+
+## Exercise INT1B
+print("Eliza Wan\nIT Project Management\n\"Hongkonger\"")
+
+import math
+print(math.factorial(3))
+
+input("Give me a value:")
+
+
+
